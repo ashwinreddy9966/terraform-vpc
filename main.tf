349ed8bd46d06229838c04c4f00ba392b4e71454
manu <manu@local.com>
2022-05-21 16:55:30 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+#module "vpc" {
+#  source = "git::https://github.com/ashwinreddy9966/tf-module-vpc.git?ref=main"
+#}
+
 module "vpc" {
-  source = "git::https://github.com/ashwinreddy9966/tf-module-vpc.git?ref=main"
+  source = "./vendor/modules/vpc"
+  VPC_CIDR = var.VPC_CIDR
 }
-
-
---
+++ vars.tf
@@ -0,0 +1,1 @@
+variables "VPC_CIDR" {}
