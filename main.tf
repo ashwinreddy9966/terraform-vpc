e6e84fbac9843d499c546c57b72cf94628e1f6fd
manu <manu@local.com>
2022-05-22 15:36:48 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@
 module "vpc" {
   source = "./vendor/modules/vpc" # we made this as the source as Terrafile places the remote source module and place here
   VPC_CIDR = var.VPC_CIDR
+  ENV      = var.ENV
 }
